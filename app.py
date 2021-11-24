7cd7aed6a215f9dca9f5993c33f863edaa6bb771
Arnaud Vuacheux <avuacheux@gmail.com>
2021-11-24 19:02:42 +0100

    website vers 2

--- app.py
+++ app.py
@@ -1,29 +1,53 @@
+# from enum import auto
 import streamlit as st
+# import altair as alt
 from PIL import Image
 import pandas as pd
-import numpy as np
+# import numpy as np
 import requests
 import re
 import time
 
-logo_style = """
-img {
+CSS = """
+etr89bj1 {
     width:150px!important;
     }
+
+.stApp {
+    background-image: url(https://images.prismic.io/ankor/7a8e937c-b781-40d2-b06e-4e4dacec4475_OMY_BANNIERE.jpg);
+    background-size: cover;
+    font-family: system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
+}
+
+h1 {
+    padding:0;
+    font-weight: 600;
+}
+
+h2 {
+    padding:0;
+}
+
+.block-container {
+    background-color:#ffffff;
+    padding: 1rem 1rem!important;
+}
 """
+
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
 
 lewagon = Image.open('images/lewagon.png')
 ankorstore = Image.open('images/ankorstore.png')
-st.write(f'<style>{logo_style}</style>', unsafe_allow_html=True)
 
 columns = st.columns(2)
 
-logo1 = columns[0].image(lewagon, use_column_width=True)
-logo2 = columns[1].image(ankorstore, use_column_width=True)
+logo1 = columns[0].image(lewagon, use_column_width=False)
+logo2 = columns[1].image(ankorstore, use_column_width=False)
 
 '''
 # Commercial 2.0
-## La qualification prédictive à partir d'un url
+## Predictive qualification from a website
 '''
 
 def regex_url(urls_list):
@@ -37,52 +61,85 @@ def regex_url(urls_list):
             i=i+1
     return i
 
-@st.cache
-def get_dataframe_data():
-    return pd.DataFrame(
-            np.random.randn(10, 5),
-            columns=('col %d' % i for i in range(5))
-        )
 
-urls = st.text_area("Inserez un ou plusieurs url(s) séparé(s) par des virgules :",
+def get_dataframe_data():
+    return pd.read_csv('data/small_cleaned_data.csv', sep=";")
+
+urls = st.text_area("Insert one or more url(s) separated by commas:",
                     "www.lewagon.com, (...)")
 urls_list = urls.split(",")
-st.write("Nombre d'url(s):", len(urls_list))
+st.write("Number of url(s):", len(urls_list))
 
-if st.button('Qualification 🎉'):
-    # print is visible in the server output, not in the page
-
+if st.button('Launch qualification 🎉'):
     bad_urls = regex_url(urls_list)
 
     if bad_urls != 0:
-        st.warning('Reste positif, certains urls ne sont peut-être pas biens écrit.')
+        st.warning('Stay positive, some urls are just maybe not well written.')
     else:
-        'Starting a long computation...'
-
         # Add a placeholder
-        latest_iteration = st.empty()
         bar = st.progress(0)
+        latest_iteration = st.empty()
 
         for i in range(100):
             # Update the progress bar with each iteration.
-            latest_iteration.text(f'Iteration {i+1}')
             bar.progress(i + 1)
-            time.sleep(0.1)
-
-        '...and now we\'re done! 🎉'
+            latest_iteration.text(f'{i+1}%')
+            time.sleep(0.001)
 
         try:
+            # Call API to get prediction
             param = {'urls': urls}
             url = 'https://api-gwwhhqf6zq-uc.a.run.app/'
             prediction = requests.get(url, params=param)
             st.write(prediction.json())
             st.success('This is a success!')
         except:
-            st.error('Let\'s keep positive, this might be pretty close to a success!')
+            # Error happen when calling API
+            st.error('Something goes wrong ! Try another url')
 
+        if len(urls_list) == 1:
+            # Display a graph if there is only one URL
+            pass
+            # df = get_dataframe_data()
+            # st.table(df.head())
+            # c = alt.Chart(df).mark_circle().encode(x='a',
+            #                                        y='b',
+            #                                        size='c',
+            #                                        color='c',
+            #                                        tooltip=['a', 'b',
+            #                                                 'c']).properties(
+            #                                                     width=700,
+            #                                                     height=500)
+            # st.write(c)
+
+        # Display a Table with the API result
         df = get_dataframe_data()
-        st.table(df.head())
+        df.drop(columns='Text_clean', inplace=True)
+        st.table(df.head(10))
 
-else:
-    pass
-    # st.write('I was not clicked 😞')
+import json
+import numpy as np
+
+
+def load_R_model(filename):
+    with open(filename, 'r') as j:
+        data_input = json.load(j)
+    data = {
+        'topic_term_dists': data_input['phi'],
+        'doc_topic_dists': data_input['theta'],
+        'doc_lengths': data_input['doc.length'],
+        'vocab': data_input['vocab'],
+        'term_frequency': data_input['term.frequency']
+    }
+    return data
+
+
+movies_model_data = load_R_model('data/movie_reviews_input.json')
+
+import pyLDAvis
+
+movies_vis_data = pyLDAvis.prepare(**movies_model_data)
+
+html_string = pyLDAvis.prepared_data_to_html(movies_vis_data)
+from streamlit import components
+components.v1.html(html_string, width=1300, height=900, scrolling=True)
