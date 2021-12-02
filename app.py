08c471cedccbb8aadbeeacacd183253e6ad099b3
Arnaud Vuacheux <avuacheux@gmail.com>
2021-12-02 14:16:55 +0100

    upload file + progress bar + linkedin pictures

--- app.py
+++ app.py
@@ -32,6 +32,11 @@ h2 {
     padding:0;
 }
 
+h3 {
+    padding:0;
+    font-weight: 600;
+}
+
 .table {
     --bs-table-striped-bg:rgb(240, 242, 246)!important;
 }
@@ -65,6 +70,7 @@ st.set_page_config(
             layout="centered")
 
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
 st.write("""<!-- CSS Bootstrap only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 """, unsafe_allow_html=True)
@@ -126,10 +132,19 @@ def highlight_score(serie, color='#09ab3b33'):
 
     return serie
 
+# Textarea with urls
 urls = st.text_area("Insert one or more url(s) separated by commas:",
                     "https://www.evaliaparis.com/, https://www.yves-rocher.fr/, https://www.uneheurepoursoi.com/, https://www.calzedonia.com/, https://newjerseyparis.com/, https://www.indies.fr/, https://bylouise.fr/, https://www.placedestendances.com/, https://www.kidsaround.com/, https://www.catimini.com/, https://www.melijoe.com/fr, https://www.sergent-major.com/")
 urls_list = urls.split(",")
 st.write("Number of url(s):", len(urls_list))
+
+# Upload a CSV
+st.set_option('deprecation.showfileUploaderEncoding', False)
+uploaded_file = st.file_uploader("Or choose a CSV file", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
+
 
 if st.button('Launch qualification 🎉'):
     bad_urls = regex_url(urls_list)
@@ -141,22 +156,35 @@ if st.button('Launch qualification 🎉'):
         bar = st.progress(0)
         latest_iteration = st.empty()
 
-        for i in range(100):
-            # Update the progress bar with each iteration.
-            bar.progress(i + 1)
-            latest_iteration.text(f'{i+1}%')
-            time.sleep(0.01)
+        # API URL local for testing / GCP in production
+        # url = 'https://api-n75dafaksa-ew.a.run.app/get_data'
+        url = 'http://127.0.0.1:8000/get_data'
 
         try:
-            # Call API to get prediction
-            param = {'urls': f'{urls_list}'}
-            url = 'https://api-n75dafaksa-ew.a.run.app/get_data'
-            # url = 'http://127.0.0.1:8000/get_data'
+            # Call API first time to get prediction
+            param = {'urls': f'["{urls_list[0]}"]'}
             prediction = requests.get(url, params=param)
-            st.success('This is a success!')
-
             data = json.loads(prediction.json())
             df = pd.json_normalize(data)
+
+            # Update the progress bar with each iteration.
+            progression = int(1/len(urls_list)*100)
+            bar.progress(progression)
+            latest_iteration.text(f'{progression}%')
+
+            for i in range(1, len(urls_list)):
+                # Call API to get prediction
+                param = {'urls': f'["{urls_list[i]}"]'}
+                prediction = requests.get(url, params=param)
+                data = json.loads(prediction.json())
+                df = pd.concat([df, pd.json_normalize(data)])
+
+                # Update the progress bar with each iteration.
+                progression = int((i+1)/len(urls_list)*100)
+                bar.progress(progression)
+                latest_iteration.text(f'{progression}%')
+
+            st.success('This is a success!')
             df.drop(columns=['Text', 'Language'], inplace=True)
             df['url'] = df['url'].apply(lambda x: '<a href="{0}" target="_blank">{0}</a>'.format(x))
             df['Email'] = df['Email'].apply(lambda x: '' if (x == 'No email' or x == 'NaN') else ", ".join(['<a href=mailto:"{0}">email</a>'.format(i) for i in x]))
@@ -171,22 +199,44 @@ if st.button('Launch qualification 🎉'):
             # Error happen when calling API
             st.error('Something went wrong ! Try another url')
 
-        if len(urls_list) == 1:
-            # Display a graph if there is only one URL
-            pass
 
-        st.markdown('# Explain our model')
+# @st.cache
+st.markdown('# Explain our model')
 
-        with st.expander("French model dataviz"):
-            # @st.cache
-            model_data_fr = load_R_model('data/dataviz_fr.json')
-            vis_data_fr = pyLDAvis.prepare(**model_data_fr)
-            html_string_fr = pyLDAvis.prepared_data_to_html(vis_data_fr)
-            components.v1.html(html_string_fr, width=1210, height=780, scrolling=False)
+# @st.cache
+with st.expander("French model dataviz"):
+    # @st.cache
+    model_data_fr = load_R_model('data/dataviz_fr.json')
+    vis_data_fr = pyLDAvis.prepare(**model_data_fr)
+    html_string_fr = pyLDAvis.prepared_data_to_html(vis_data_fr)
+    components.v1.html(html_string_fr, width=1210, height=780, scrolling=False)
 
-        with st.expander("English model dataviz"):
-            # @st.cache
-            model_data_en = load_R_model('data/dataviz_en.json')
-            vis_data_en = pyLDAvis.prepare(**model_data_en)
-            html_string_en = pyLDAvis.prepared_data_to_html(vis_data_en)
-            components.v1.html(html_string_en, width=1210, height=780, scrolling=False)
+# @st.cache
+with st.expander("English model dataviz"):
+    # @st.cache
+    model_data_en = load_R_model('data/dataviz_en.json')
+    vis_data_en = pyLDAvis.prepare(**model_data_en)
+    html_string_en = pyLDAvis.prepared_data_to_html(vis_data_en)
+    components.v1.html(html_string_en, width=1210, height=780, scrolling=False)
+
+st.markdown("""#### Made with ❤️ by""")
+col1, col2, col3 = st.columns(3)
+
+col1.write("""<div style='text-align: center;'>
+            <a href='https://www.linkedin.com/in/arnaud-vuacheux/' target='_blank'>
+                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQFKsogocxpUTA/profile-displayphoto-shrink_800_800/0/1634035549772?e=1643846400&v=beta&t=Gf-YYd1p3rkz8abPd59txkO2dmLMFQOS6jglDZhFTcg' style='width:120px;height:120px;border-radius:50%;margin-bottom: 25px;' />
+                <p>Arnaud Vuacheux</p>
+            </a>
+            </div>""", unsafe_allow_html = True)
+col2.write("""<div style='text-align: center;'>
+            <a href='https://www.linkedin.com/in/emmanuel-miralles-a2604911a/' target='_blank'>
+            <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEpDVzZZTEwyQ/profile-displayphoto-shrink_800_800/0/1616948728275?e=1643846400&v=beta&t=st95nKH408-cpnsav0fbM0YWRHIEXphxxKxS0MowdEo' style='width:120px;height:120px;border-radius:50%;margin-bottom: 25px;' />
+            <p>Emmanuel Miralles</p>
+            </a>
+            </div>""", unsafe_allow_html = True)
+col3.write("""<div style='text-align: center;'>
+            <a href='https://www.linkedin.com/in/robinbusinessdevelopment/'target='_blank'>
+            <img src='https://media-exp1.licdn.com/dms/image/C4E03AQH73LCrsyPnJA/profile-displayphoto-shrink_800_800/0/1636019621502?e=1643846400&v=beta&t=bSTvj5JS8yPRnjET9Y6ybHmS8TXQdLuAo_Tstn2G1lw' style='width:120px;height:120px;border-radius:50%;margin-bottom: 25px;' />
+            <p>Robin Lespagnol</p>
+            </a>
+            </div>""", unsafe_allow_html = True)
